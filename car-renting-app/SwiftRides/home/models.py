7da816860372dad83a1b1046086cf91636d70c1b
chethan <chethanchethu99339@gmail.com>
2024-03-12 21:58:50 +0530

    migrations file is updated

--- car-renting-app/SwiftRides/home/models.py
+++ car-renting-app/SwiftRides/home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class homes(models.Model):
+    FirstName = models.CharField(max_length=255)
+    secondName = models.CharField(max_length=255)
+    phone = models.IntegerField(null = True)
+    dateOfBirth = models.DateField(null = True)
+
